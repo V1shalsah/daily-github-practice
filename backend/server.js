117cedbbef4aa31b8e3c8c7eee02d84ad08e0250
vishal sah <vishalsah1501@gmail.com>
2026-03-08 13:07:31 +0530

    Add backend server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
 
-app.get("/", (req,res)=>{
-    res.send("Backend Working");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend server running successfully");
 });
 
-app.listen(5000,()=>{
-    console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
