fd488fa5caf27907359935bf0418a509304824c3
vishal sah <vishalsah1501@gmail.com>
2026-03-10 22:35:28 +0530

    Add MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,22 @@ app.get("/", (req, res) => {
   res.send("Backend server running");
 });
 
+
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+app.use(express.json());
+
+// connect database
+connectDB();
+
+app.use("/api", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend server running");
+});
+
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
